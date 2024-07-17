76d01656cbb178c9ba7c000ffa6e71f3de76c53d
Harsh2002kumar <harshkumarpandey2002@gmail.com>
2024-07-17 14:54:12 +0530

    100% code coverage

---
+++ src/app/app.model.ts
@@ -0,0 +1,53 @@
+export const initialData = [
+  {
+    id: 1,
+    name: 'John Doe',
+    workouts: [
+      { type: 'Running', minutes: 30 },
+      { type: 'Cycling', minutes: 45 },
+    ],
+    totalWorkouts: 2,
+    totalMinutes: 75,
+  },
+  {
+    id: 2,
+    name: 'Jane Smith',
+    workouts: [
+      { type: 'Swimming', minutes: 60 },
+      { type: 'Running', minutes: 20 },
+    ],
+    totalWorkouts: 2,
+    totalMinutes: 80,
+  },
+  {
+    id: 3,
+    name: 'Mike Johnson',
+    workouts: [
+      { type: 'Yoga', minutes: 50 },
+      { type: 'Cycling', minutes: 40 },
+    ],
+    totalWorkouts: 2,
+    totalMinutes: 90,
+  },
+  {
+    id: 4,
+    name: 'Harsh Kumar',
+    workouts: [{ type: 'Cycling', minutes: 50 }],
+    totalWorkouts: 1,
+    totalMinutes: 50,
+  },
+  {
+    id: 5,
+    name: 'Johnson',
+    workouts: [{ type: 'Swimming', minutes: 40 }],
+    totalWorkouts: 1,
+    totalMinutes: 40,
+  },
+  {
+    id: 6,
+    name: 'Smith',
+    workouts: [{ type: 'Yoga', minutes: 40 }],
+    totalWorkouts: 1,
+    totalMinutes: 40,
+  },
+];
